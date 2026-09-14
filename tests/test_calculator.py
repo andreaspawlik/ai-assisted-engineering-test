348e417d87474a0c0c78c1bc8de2a3fda005c4b0
andreaspawlik <andreas.pawlik@gmail.com>
2026-09-14 07:33:38 +0200

    Add subtract command
    Closes #1

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,4 @@
-from calculator.__main__ import add, main
+from calculator.__main__ import add, main, subtract
 
 
 def test_add():
@@ -8,3 +8,12 @@ def test_add():
 def test_cli_add(capsys):
     assert main(["add", "2", "3"]) == 0
     assert capsys.readouterr().out.strip() == "5.0"
+
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+
+def test_cli_subtract(capsys):
+    assert main(["subtract", "5", "3"]) == 0
+    assert capsys.readouterr().out.strip() == "2.0"
